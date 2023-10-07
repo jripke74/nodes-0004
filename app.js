7f905e6627592c11dee8d5afbe9de4b091bf4a90
Jeff Ripke <jripke74@gmail.com>
2023-10-07 09:39:07 -0700

    add csrf tokens

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
 
 const mongoDbUsername = require("./util/secrets").mongoDbUsername;
 const mongoDbPassword = require("./util/secrets").mongoDbPassword;
@@ -19,6 +20,7 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: "sessions",
 });
+const csrfProtection = csrf();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -37,6 +39,7 @@ app.use(
     store: store,
   })
 );
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
