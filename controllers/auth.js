4a051ad32ceb3549f1d72e4c4cd62179b48bae5e
Jeff Ripke <jripke74@gmail.com>
2023-10-12 10:54:24 -0700

    add email validation to auth route

--- controllers/auth.js
+++ controllers/auth.js
@@ -81,48 +81,35 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors.array());
     return res.status(422).render("auth/signup", {
       path: "/signup",
-      pageTitle: 'Signup',
-      errorMessage: errors.array()[0],
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
     });
   }
-  User.findOne({ email: email })
-    .then((userDoc) => {
-      if (userDoc) {
-        req.flash(
-          "error",
-          "E-mail exists already, please pick a different one."
-        );
-        return res.redirect("/signup");
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] },
-          });
-          return user.save();
-        })
-        .then((result) => {
-          res.redirect("/login");
-          console.log(myEmail);
-          return transporter.sendMail({
-            to: email,
-            from: myEmail,
-            subject: "Signup succeeded!",
-            html: "<h1>You successfully signed up!</h1>",
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] },
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+      console.log(myEmail);
+      return transporter.sendMail({
+        to: email,
+        from: myEmail,
+        subject: "Signup succeeded!",
+        html: "<h1>You successfully signed up!</h1>",
+      });
     })
     .catch((err) => {
       console.log(err);
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { check, body } = require("express-validator");
 
 const authController = require("../controllers/auth");
+const User = require("../models/user");
+const user = require("../models/user");
 
 const router = express.Router();
 
@@ -11,21 +13,34 @@ router.get("/signup", authController.getSignup);
 
 router.post("/login", authController.postLogin);
 
-router.post("/signup", [
-  check("email").isEmail().withMessage("Please enter a valid email."),
-  body(
-    "password",
-    "Please enter a password with only numbers and letters and at least 5 characters."
-  )
-    .isLength({ min: 5 })
-    .isAlphanumeric(),
-  body('confirmPassword').custom((value, { req  }) => {
-    if (value !== req.body.password) {
-      throw new Error("Password have to match!");
-    }
-  }),
-],
-  authController.postSignup,
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              "E-mail exists already please pick a different one."
+            );
+          }
+        });
+      }),
+    body(
+      "password",
+      "Please enter a password with only numbers and letters and at least 5 characters."
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password have to match!");
+      }
+    }),
+  ],
+  authController.postSignup
 );
 
 router.post("/logout", authController.postLogout);
