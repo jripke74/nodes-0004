a5c855155bd4f38559368220aaf50a6a91cea53f
Jeff Ripke <jripke74@gmail.com>
2023-10-17 11:48:25 -0700

    add error middleware

--- controllers/admin.js
+++ controllers/admin.js
@@ -55,7 +55,9 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect("/admin/products");
     })
     .catch((err) => {
-      res.redirect('/500');
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -67,6 +69,7 @@ exports.getEditProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
     .then((product) => {
+      throw new Error('Dummy');
       if (!product) {
         return res.redirect("/");
       }
@@ -80,7 +83,11 @@ exports.getEditProduct = (req, res, next) => {
         validationErrors: [],
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postEditProduct = (req, res, next) => {
