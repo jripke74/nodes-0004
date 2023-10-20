65787b1b55240c7424ffd65dfeccd014311c09f0
Jeff Ripke <jripke74@gmail.com>
2023-10-20 10:50:47 -0700

    add product delete in DOM

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,23 @@
 const deleteProduct = (btn) => {
   const prodId = btn.parentNode.querySelector("[name=productId").value;
   const csrf = btn.parentNode.querySelector("[name=_csrf").value;
+
+  const productElement = btn.closest("article");
+
+  fetch("/admin/product/" + prodId, {
+    method: "DELETE",
+    headers: {
+      "csrf-token": csrf,
+    },
+  })
+    .then((result) => {
+      console.log(result);
+    })
+    .then(data => {
+      console.log(data);
+      productElement.parentNode.removeChild(productElement); // works in all browser inc. IE
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
